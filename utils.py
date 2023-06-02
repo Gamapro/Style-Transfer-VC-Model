a791954bdc839f4f79adaf589fb616909f1a66d5
David Gamaliel Arcos <61252778+Gamapro@users.noreply.github.com>
2023-06-01 23:48:48 -0600

    xd
    xd

--- utils.py
+++ utils.py
@@ -33,27 +33,27 @@ class AudioDataset(Dataset):
     
 
     class Wave_Block(nn.Module):
-    def __init__(self, in_channels, out_channels, dilation_rates, kernel_size):
-        super(Wave_Block, self).__init__()
-        self.num_rates = dilation_rates
-        self.convs = nn.ModuleList()
-        self.filter_convs = nn.ModuleList()
-        self.gate_convs = nn.ModuleList()
+        def __init__(self, in_channels, out_channels, dilation_rates, kernel_size):
+            super(Wave_Block, self).__init__()
+            self.num_rates = dilation_rates
+            self.convs = nn.ModuleList()
+            self.filter_convs = nn.ModuleList()
+            self.gate_convs = nn.ModuleList()
 
-        self.convs.append(nn.Conv1d(in_channels, out_channels, kernel_size=1))
-        dilation_rates = [2 ** i for i in range(dilation_rates)]
-        for dilation_rate in dilation_rates:
-            self.filter_convs.append(
-                nn.Conv1d(out_channels, out_channels, kernel_size=kernel_size, padding=int((dilation_rate*(kernel_size-1))/2), dilation=dilation_rate))
-            self.gate_convs.append(
-                nn.Conv1d(out_channels, out_channels, kernel_size=kernel_size, padding=int((dilation_rate*(kernel_size-1))/2), dilation=dilation_rate))
-            self.convs.append(nn.Conv1d(out_channels, out_channels, kernel_size=1))
+            self.convs.append(nn.Conv1d(in_channels, out_channels, kernel_size=1))
+            dilation_rates = [2 ** i for i in range(dilation_rates)]
+            for dilation_rate in dilation_rates:
+                self.filter_convs.append(
+                    nn.Conv1d(out_channels, out_channels, kernel_size=kernel_size, padding=int((dilation_rate*(kernel_size-1))/2), dilation=dilation_rate))
+                self.gate_convs.append(
+                    nn.Conv1d(out_channels, out_channels, kernel_size=kernel_size, padding=int((dilation_rate*(kernel_size-1))/2), dilation=dilation_rate))
+                self.convs.append(nn.Conv1d(out_channels, out_channels, kernel_size=1))
 
-    def forward(self, x):
-        x = self.convs[0](x)
-        res = x
-        for i in range(self.num_rates):
-            x = torch.tanh(self.filter_convs[i](x)) * torch.sigmoid(self.gate_convs[i](x))
-            x = self.convs[i + 1](x)
-            res = res + x
-        return res
+        def forward(self, x):
+            x = self.convs[0](x)
+            res = x
+            for i in range(self.num_rates):
+                x = torch.tanh(self.filter_convs[i](x)) * torch.sigmoid(self.gate_convs[i](x))
+                x = self.convs[i + 1](x)
+                res = res + x
+            return res
